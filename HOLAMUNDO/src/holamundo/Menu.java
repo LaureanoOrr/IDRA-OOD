3d7fa38628268ed1e9d32ac0a9a19f917a82f657
Laureano Orr <laureano.orr@hotmail.com>
2023-05-22 20:14:05 -0300

    Agregar clase persona

--- HOLAMUNDO/src/holamundo/Menu.java
+++ HOLAMUNDO/src/holamundo/Menu.java
@@ -8,13 +8,11 @@ public class Menu {
    Consola teclado;
    
    
-   
-   
     public void MostrarMenu() throws InterruptedException 
     {
     int opcion;
-        
-    do {    
+        do {  
+    
     System.out.println("Bievenido al menu de la agenda. A continuación se le mostraran las opciones correspondientes.");
     TimeUnit.SECONDS.sleep(2);
     System.out.println("Opción 1: Leer datos de una persona.");
@@ -22,23 +20,22 @@ public class Menu {
     System.out.println("Opción 3: Listar todas las personas.");
     System.out.println("Opcion 4: Salir");
     opcion = teclado.leerEntero("Ingrese la opcion deseada.");
-    
-    
-    switch(opcion){
+            
+    switch(opcion)
+    {
         case 1:
             break;
+            
         case 2:
             break;
+            
         case 3:
             break;
     }
-    while (opcion != 4);
+            
     
-     }
+    
+        } while (opcion != 4);
+    
     }
-    
 }
-
-
-
-
--- HOLAMUNDO/src/holamundo/Persona.java
+++ HOLAMUNDO/src/holamundo/Persona.java
@@ -1,13 +1,63 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package holamundo;
 
-/**
- *
- * @author Alumno30
- */
+
 public class Persona {
+    protected String nombre;
+    protected String apellido;
+    protected int documento;
+    // protected LocalDate fechaDeNacimiento;
+    
+    public Persona(String nombre, String apellido)
+    {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    }
+    
+    public Persona(String nombre, String apellido, int documento)
+    {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.documento = documento;
+    }
+    
+    public Persona()
+    {
+        this.nombre = "";
+        this.apellido = "";
+    }
+    
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public String getApellido()
+    {
+        return this.apellido;
+    }
+    
+    public void setApellido(String apellido)
+    {
+        this.apellido = apellido;
+    }
+    
+    public int getDocumento()
+    {
+        return this.documento;
+    }
+    
+    public void setDocumento(int documento)
+    {
+        this.documento = documento;
+    }
+    
+    
+    }
     
 }
