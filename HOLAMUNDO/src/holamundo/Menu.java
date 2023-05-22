cf70040ccef1635a0eaf20b19ebfb9d3cab85d20
Laureano Orr <laureano.orr@hotmail.com>
2023-05-22 20:48:07 -0300

    Agregar cambio de menu

--- HOLAMUNDO/src/holamundo/Menu.java
+++ HOLAMUNDO/src/holamundo/Menu.java
@@ -5,7 +5,16 @@ import java.util.concurrent.TimeUnit;
 
 public class Menu {
      
-   Consola teclado;
+   protected Consola teclado;
+   protected Persona arregloGente[];
+   protected int cantGente = 0;
+   
+   public Menu()
+   {
+       this.teclado = new Consola();
+      arregloGente = new Persona[10];
+      cantGente = 0;
+   }
    
    
     public void MostrarMenu() throws InterruptedException 
@@ -15,7 +24,7 @@ public class Menu {
     
     System.out.println("Bievenido al menu de la agenda. A continuación se le mostraran las opciones correspondientes.");
     TimeUnit.SECONDS.sleep(2);
-    System.out.println("Opción 1: Leer datos de una persona.");
+    System.out.println("Opción 1: Ingresar datos de una persona y guardarlo en la agenda.");
     System.out.println("Opción 2: Buscar datos de una persona por documento.");
     System.out.println("Opción 3: Listar todas las personas.");
     System.out.println("Opcion 4: Salir");
@@ -24,6 +33,7 @@ public class Menu {
     switch(opcion)
     {
         case 1:
+            System.out.println("Elegiste la opción 1.");
             break;
             
         case 2:
@@ -38,4 +48,23 @@ public class Menu {
         } while (opcion != 4);
     
     }
+    
+    public void LeerDatos()
+    {
+        for (int i = 0; i < arregloGente.length; i++) {
+            System.out.println("Estas en la posición " + i + ".");
+            
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+    
 }
